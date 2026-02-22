6d72bbaf763be90400bb01fd69160c78cead40a3
Nityanutan mishra <mishra.two1@gmail.com>
2026-02-23 00:45:46 +0530

    ...

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import {
   Dashboard,
@@ -18,21 +18,21 @@ import {
 
 const AppRouter = () => {
   return (
-    <Router basename="/react-vite-deploy">
+    <Router>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/invoices" element={<Invoices />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/bar" element={<Bar />} />
-          <Route path="/pie" element={<Pie />} />
-          <Route path="/stream" element={<Stream />} />
-          <Route path="/line" element={<Line />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/geography" element={<Geography />} />
+          <Route index element={<Dashboard />} />
+          <Route path="team" element={<Team />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="invoices" element={<Invoices />} />
+          <Route path="form" element={<Form />} />
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="bar" element={<Bar />} />
+          <Route path="pie" element={<Pie />} />
+          <Route path="stream" element={<Stream />} />
+          <Route path="line" element={<Line />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="geography" element={<Geography />} />
         </Route>
       </Routes>
     </Router>
